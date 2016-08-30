cf1dad2c6260576ce46624f811305271a43647f6
Peter Camilleri <peter.c.camilleri@gmail.com>
2016-08-30 11:59:53 -0400

    The rake console command now uses the standard readline gem.

--- irbt.rb
+++ irbt.rb
@@ -2,8 +2,6 @@
 # An IRB + ruby_sscanf test bed
 
 require 'irb'
-$force_alias_read_line_module = true
-require 'mini_readline'
 
 puts "Starting an IRB console with ruby_sscanf loaded."
 
--- lib/ruby_sscanf/version.rb
+++ lib/ruby_sscanf/version.rb
@@ -1,5 +1,5 @@
 #The ruby_sscanf doesn't really live here.
 module RubySscanf
   #The gem's version.
-  VERSION = "0.2.3"
+  VERSION = "0.2.4"
 end
